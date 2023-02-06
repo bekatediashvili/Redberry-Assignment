b7033dfae22621fbbe954f2e2d768b677b1fc62a
tatas <bekatediashvili1995@gmail.com>
2023-02-07 02:09:11 +0400

    State is Pinia Store

---
+++ src/store/Global.js
@@ -0,0 +1,23 @@
+  import { defineStore } from "pinia";
+//
+//
+    export const GlobalStore = defineStore('GlobalStore', {
+        state: () => ({
+            surname: '',
+            name: '',
+            email: '',
+            phone: '',
+            about: '',
+            image: '',
+            position: '',
+            employer: '',
+            begginningDate: '',
+            endData: '',
+
+
+        }),
+
+    })
+
+
+
