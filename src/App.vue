48fbf2e99350edfc4a76ff57e1291491730091e8
tatas <bekatediashvili1995@gmail.com>
2023-02-07 02:10:40 +0400

    State is Pinia Store

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Vue app</h1>
+<!--<personal-info></personal-info>-->
+
+<!--  <Home></Home>-->
+
+  <RouterView></RouterView>
+<!--<test></test>-->
+
+
+
 </template>
+
+<script setup>
+import PersonalInfo from "./components/PersonalInfo.vue";
+import Experience from "./components/Experience.vue";
+import Home from "./components/Home.vue";
+import test from "./components/test.vue"
+import Test from "./components/test.vue";
+
+</script>
+<style scoped>
+
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import {createApp} from 'vue'
 import './style.css'
+import router from "./router/index.js";
 import {createPinia} from "pinia";
 import App from './App.vue'
 
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(router)
 app.mount('#app')
