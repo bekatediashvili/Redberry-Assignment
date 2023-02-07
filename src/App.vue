fbb22e574aa4f4daf8f966981e31d6e992750a68
tatas <bekatediashvili1995@gmail.com>
2023-02-07 17:55:53 +0400

    added fixed & clean app.vue file

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <RouterView></RouterView>
+</template>
+
+<script setup>
+import PersonalInfo from "./components/PersonalInfo.vue";
+import Experience from "./components/Experience.vue";
+import Home from "./components/Home.vue";
+import test from "./components/test.vue"
+import Test from "./components/test.vue";
+
+</script>
+<style scoped>
+</style>
+
