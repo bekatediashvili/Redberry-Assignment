dd8a52d3e037829f2a63eb444a7dbb489063f9af
tatas <bekatediashvili1995@gmail.com>
2023-02-07 22:44:01 +0400

    feature: all routes,

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,12 @@ import {createRouter, createWebHistory,} from "vue-router";
 
 // routes
 import Home from "../components/Home.vue";
+import PersonalInfo from "../components/PersonalInfo.vue";
+import test from "../components/test.vue";
+import Experience from "../components/Experience.vue";
+import Education from "../components/Education.vue";
 import NotFound from "../components/NotFound.vue";
+import Resume from "../components/Resume.vue";
 
 const routes = [
 
@@ -11,8 +16,17 @@ const routes = [
         path: '/',
         redirect: '/home',
     },
+
     {path: '/home', component: Home},
+    {path: '/personalinfo', component: PersonalInfo},
+    {path: '/experience', component: Experience},
+    {path: '/education', component: Education},
+    {
+        path: '/resume',
+        component: Resume
+    },
     {path: '/:notFound(.*)', component: NotFound}
+
 
 
 ]
